81de421adcd32550932efbd3f2e46d83243d1196
dong.wang <dong.wang@yingzt.com>
2019-02-15 16:01:17 +0800

    餐厅点评完善

---
+++ js/service-worker.js
@@ -0,0 +1,55 @@
+var staticCacheName = 'homework-v2';
+
+self.addEventListener('install', event => {
+    console.log('v2 installing…');
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                '/js/dbhelper.js',
+                '/js/main.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => {
+                    return cacheName.startsWith('homework-') &&
+                        cacheName != staticCacheName;
+                }).map(cacheName => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('message', event => {
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
