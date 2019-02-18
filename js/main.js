46a570c85db935891ead9fe638ddc6225303d6ff
dong.wang <dong.wang@yingzt.com>
2019-02-18 10:37:52 +0800

    根据老师审阅，修改完善代码

--- js/main.js
+++ js/main.js
@@ -177,9 +177,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `Photo of ${restaurant.name} restaurant`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
